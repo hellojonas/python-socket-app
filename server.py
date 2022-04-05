ee1cdc07e9bd8b7043b4540b7f0f350b9f08f176
Hello Jonas <hellojonashp@gmail.com>
2022-04-05 21:53:26 +0200

    complete refactor of server

--- server.py
+++ server.py
@@ -3,80 +3,92 @@ import threading
 from time import sleep
 
 CHUNK = 1024
+MAX_CONNECTION = 1
 
-class StopServerException(Exception):
-    def __init__(self):
-        self.message = "Server has been stopped"
-        super().__init__(self.message)
+class StopWatcherException(Exception):
+    def __init__(self, msg='server has been stopped'):
+        self.message = msg
+        super().__init__(msg)
+
+class StopWatchException(Exception):
+    def __init__(self, msg='server has stopped watching messages'):
+        self.message = msg
+        super().__init__(msg)
  
-class Server:
-    _server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    def __init__(self, host="localhost", port=8000, max_conn=1):
+class Server():
+    def __init__(self, host="localhost", port=8000):
+        self._server = None
         self._callback = None
         self._client = None 
+        self._running = False
         self._host = host
         self._port = port
-        self._max_conn = max_conn
-        self._server.bind((host, port))
-        self._server.listen(max_conn)
-        self._t_server = None
-        self._t_message = None
-        self._running = False
-
-    def config(self, host, port):
-        self.host = host
-        self.port = port
-        return self
+        self.thread_server = None
+        self.thread_msg = None
 
     def _accept(self):
+        # with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self._host, self._port))
+        s.listen(MAX_CONNECTION)
+        self._server = s
         while True:
-            conn, addr = self._server.accept()
-            # _watch_client(conn)
-            print(f'client with ip {addr} connected')
-            self._client = conn
+            try:
+                print("[accept]: Waiting for connection")
+                conn, addr = s.accept()
+                print(f"C[accept]: lient with ip {addr} is connected")
+                self._client = conn
+            except:
+                print("[accept]: Cannot accept connections")
+                print("[accept]: Removing client")
+                self._client = None
+                break
 
-    def start(self):
-        print(f"Server running on {self._host}:{self._port}")
-        self._t_server = threading.Thread(target=self._accept)
-        self._t_server.start()
+    def bind(self, host, port):
+        self._host = host
+        self._port = port
+        return self
+
+    def run(self):
         self._running = True
-        self._t_message = threading.Thread(target=self._watch_client)
-        self._t_message.start()
-        if not self._running:
-            raise StopServerException()
-        return self._t_server
+        print(f"[run]: Server listening on {self._host}:{self._port}")
+        self.thread_server = threading.Thread(target=self._accept)
+        self.thread_server.start()
+        self.thread_msg = threading.Thread(target=self._on_message)
+        self.thread_msg.start()
+        return self
 
     def stop(self):
+        print("[stop]: stopping server")
         self._running = False
+        self._server.shutdown(socket.SHUT_RDWR)
         self._server.close()
 
     def register_cb(self, cb):
         self._callback = cb
 
-    
-    def _watch_client(self):
-        def on_message():
-            print("[watch_cliet]: loop----\n")
+    def _on_message(self):
+        while True:
             try:
                 msg = self._client.recv(CHUNK).decode("utf-8")
+                print(f"[on_message]: received {msg}")
                 if not msg or msg == "":
-                    return
+                    sleep(1)
+                    continue
                 self._callback(msg)
+                print(f"[on_message]: sending '{msg}'")
             except:
-                pass
-        while True:
-            on_message()
-            sleep(1)
-
-def init(host, port, conn_num=1):
-    return Server(host, port, max_conn=conn_num)
+                if not self._running:
+                    print(f"[on_message]: stopping watching for messages")
+                    break
+                print("[on_message]: watching...")
+                sleep(1)
+                # if self._client:
+                #     self._client.close()
+                # raise StopWatcherException()
 
 if __name__ == "__main__":
-    def print_cb(msg):
-        print(f"[print_cb]: {msg}")
-    myserver = Server()
-    myserver.register_cb(print_cb)
-    myserver.start()
-    myserver.stop()
-    print("server running")
-
+    server = Server().bind("localhost", 8000).run()
+    # threading.Timer(5, server.stop).start()
+    # threading.Timer(10, server.run).start()
+    # threading.Timer(15, server.stop).start()
